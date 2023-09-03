e81b6e812c3e12376ebb85257aeb07c1535bc53f
chuannarongvat <narongvat.chingpayakmon@student.uts.edu.au>
2023-09-04 00:37:09 +1000

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A short description of the project.',
+    author='narongvat chingpayakmon/UTS/internship_c_dataset_2',
+    license='MIT',
+)
